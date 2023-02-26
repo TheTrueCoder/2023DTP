86e42cfdb9b610da698fa5c6357e2e05491b40da
Nathan <thetruecoderman@gmail.com>
2023-02-26 21:33:30 +1300

    Add big ol load of sprites

---
+++ Python/GameLearning/Sprites/Kenny/Kenny items.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Kenny Items" tilewidth="128" tileheight="128" tilecount="21" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="items/coinBronze.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="items/coinGold.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="items/coinSilver.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="items/flagGreen_down.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="items/flagGreen1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="items/flagGreen2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="items/flagRed_down.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="items/flagRed1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="items/flagRed2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="items/flagYellow_down.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="items/flagYellow1.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="items/flagYellow2.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="items/gemBlue.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="items/gemGreen.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="items/gemRed.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="items/gemYellow.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="items/keyBlue.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="items/keyGreen.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="items/keyRed.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="items/keyYellow.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="items/star.png"/>
+ </tile>
+</tileset>
---
+++ Python/GameLearning/Sprites/MagicCliffs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="MagicCavern" tilewidth="16" tileheight="16" tilecount="1160" columns="58">
+ <image source="Magic-Cliffs-Gamekit/Environment/PNG/tileset.png" width="928" height="320"/>
+</tileset>
