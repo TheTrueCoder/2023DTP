abd60aa4ff9ad51249a10b98f5bb129d82c3879f
Nathan <thetruecoderman@gmail.com>
2023-03-25 21:44:55 +1300

    Add background

---
+++ Python/BurialBandit/assets/JungleRuins/JungleBackground.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="JungleBackground" tilewidth="336" tileheight="240" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="112" height="240" source="back.png"/>
+ </tile>
+ <tile id="2">
+  <image width="336" height="240" source="middle-a.png"/>
+ </tile>
+ <tile id="3">
+  <image width="288" height="240" source="middle-b.png"/>
+ </tile>
+</tileset>
