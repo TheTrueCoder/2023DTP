f33ad80f7e19ebf2c8e76d906c23717c5d47c0e1
Nathan <thetruecoderman@gmail.com>
2023-03-25 15:29:38 +1300

    Add pickup assets

--- Python/BurialBandit/assets/pickups/AnimatedPickups.tsx
+++ Python/BurialBandit/assets/pickups/AnimatedPickups.tsx
@@ -1,16 +1,96 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="AnimatedPickups" tilewidth="32" tileheight="32" tilecount="120" columns="8">
- <image source="animated_items.png" width="256" height="480"/>
+<tileset version="1.9" tiledversion="1.9.2" name="AnimatedPickups" tilewidth="32" tileheight="32" tilecount="24" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="112">
+  <image width="32" height="32" source="animated_items/0_0.png"/>
+ </tile>
+ <tile id="113">
+  <image width="32" height="32" source="animated_items/32_0.png"/>
+ </tile>
+ <tile id="114">
+  <image width="32" height="32" source="animated_items/64_0.png"/>
+ </tile>
+ <tile id="115">
+  <image width="32" height="32" source="animated_items/96_0.png"/>
+ </tile>
+ <tile id="116">
+  <image width="32" height="32" source="animated_items/128_0.png"/>
+ </tile>
+ <tile id="117">
+  <image width="32" height="32" source="animated_items/160_0.png"/>
+ </tile>
+ <tile id="118">
+  <image width="32" height="32" source="animated_items/192_0.png"/>
+ </tile>
+ <tile id="119">
+  <image width="32" height="32" source="animated_items/224_0.png"/>
+ </tile>
+ <tile id="120">
+  <image width="32" height="32" source="animated_items/0_448.png"/>
   <animation>
-   <frame tileid="112" duration="125"/>
-   <frame tileid="113" duration="125"/>
-   <frame tileid="114" duration="125"/>
-   <frame tileid="115" duration="125"/>
-   <frame tileid="116" duration="125"/>
-   <frame tileid="117" duration="125"/>
-   <frame tileid="118" duration="125"/>
-   <frame tileid="119" duration="125"/>
+   <frame tileid="120" duration="125"/>
+   <frame tileid="121" duration="125"/>
+   <frame tileid="122" duration="125"/>
+   <frame tileid="123" duration="125"/>
+   <frame tileid="124" duration="125"/>
+   <frame tileid="125" duration="125"/>
+   <frame tileid="126" duration="125"/>
+   <frame tileid="127" duration="125"/>
   </animation>
+ </tile>
+ <tile id="121">
+  <image width="32" height="32" source="animated_items/32_448.png"/>
+ </tile>
+ <tile id="122">
+  <image width="32" height="32" source="animated_items/64_448.png"/>
+ </tile>
+ <tile id="123">
+  <image width="32" height="32" source="animated_items/96_448.png"/>
+ </tile>
+ <tile id="124">
+  <image width="32" height="32" source="animated_items/128_448.png"/>
+ </tile>
+ <tile id="125">
+  <image width="32" height="32" source="animated_items/160_448.png"/>
+ </tile>
+ <tile id="126">
+  <image width="32" height="32" source="animated_items/192_448.png"/>
+ </tile>
+ <tile id="127">
+  <image width="32" height="32" source="animated_items/224_448.png"/>
+ </tile>
+ <tile id="128">
+  <image width="32" height="32" source="animated_items/0_416.png"/>
+  <animation>
+   <frame tileid="128" duration="125"/>
+   <frame tileid="129" duration="125"/>
+   <frame tileid="130" duration="125"/>
+   <frame tileid="131" duration="125"/>
+   <frame tileid="132" duration="125"/>
+   <frame tileid="133" duration="125"/>
+   <frame tileid="134" duration="125"/>
+   <frame tileid="135" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="129">
+  <image width="32" height="32" source="animated_items/32_416.png"/>
+ </tile>
+ <tile id="130">
+  <image width="32" height="32" source="animated_items/64_416.png"/>
+ </tile>
+ <tile id="131">
+  <image width="32" height="32" source="animated_items/96_416.png"/>
+ </tile>
+ <tile id="132">
+  <image width="32" height="32" source="animated_items/128_416.png"/>
+ </tile>
+ <tile id="133">
+  <image width="32" height="32" source="animated_items/160_416.png"/>
+ </tile>
+ <tile id="134">
+  <image width="32" height="32" source="animated_items/192_416.png"/>
+ </tile>
+ <tile id="135">
+  <image width="32" height="32" source="animated_items/224_416.png"/>
  </tile>
 </tileset>
