046df7fba98456fda02dfe6c41f9cad48521200f
TheTrueCoder <thetruecoderman@gmail.com>
2023-06-08 15:10:00 +1200

    Add news page and svelte test page

---
+++ Websites/SvelteUITest/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { AppShell, Navbar, Header, Aside, Footer, ShellSection } from '@svelteuidev/core';
+</script>
+
+<AppShell>
+    <Navbar>
+        <h1>Hello Fam!</h1>
+    </Navbar>
+    <Aside>
+        Hello
+    </Aside>
+    <Footer>
+        The bottom.
+    </Footer>
+</AppShell>
